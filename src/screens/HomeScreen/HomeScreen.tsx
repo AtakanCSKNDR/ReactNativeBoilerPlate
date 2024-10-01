725280730a5fc10348d023d75587953f409777bf
ATAKAN COSKUNDERE <atakancoskundere@ticimax.com>
2024-10-02 01:04:31 +0300

    vector icon library added

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,22 +1,30 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Octicons';
+import BPText from '../../core/atomic/atoms/BPText';
 
 const HomeScreen = () => {
-  const navigation:any  = useNavigation()
-  const handleOpen = () => { 
-    navigation.navigate("BottomDrawerScreen" , {title:"Test" , subtitle:"Test Subtitle" , children:<Text>Lorem Ipsum Dolor Sit Amet</Text>})
-  }
+  const navigation: any = useNavigation();
+  const handleOpen = () => {
+    navigation.navigate('BottomDrawerScreen', {
+      title: 'Test',
+      subtitle: 'Test Subtitle',
+      children: <Text>Lorem Ipsum Dolor Sit Amet</Text>,
+    });
+  };
   return (
-    <View>
-      
-      <TouchableOpacity onPress={handleOpen}>
-        <Text>Open Drawer</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <BPText type="title" text="Anasayfa"/>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flex: 1,
+  },
+});
